a0816d54fca4a265909ae31bbf424ba7cc70a9bd
nadavsholev-ur <nadav@unlimited-robotics>
2021-09-09 10:59:20 +0300

    added moveit commander dependancy

--- scripts/arms_interface.py
+++ scripts/arms_interface.py
@@ -22,36 +22,36 @@ from tf.transformations import quaternion_from_euler
 from raya_arms_planner.RayaArmsPlanner import JointGoalPlanner, PoseGoalPlanner
 
 if __name__ == "__main__":
-    # rospy.init_node("arm_interface_action_server")
-    # joint_action_server = JointGoalPlanner()
-    # pose_action_server = PoseGoalPlanner()
-    # rospy.loginfo("Arms Interface Action Server Initialized")
-    # try:
-    #     rospy.spin()
-    # except KeyboardInterrupt:
-    #     print("keyboard interupt detected, exiting...")
-    #     exit()
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('raya_arms_interface', anonymous=True)
-    robot = RobotCommander()
-    scene = PlanningSceneInterface()
-    left_move_group = MoveGroupCommander("left_arm")
-    right_move_group = MoveGroupCommander("right_arm")
-    goal_pose = Pose()
-    goal_pose.position.x = 0.28
-    goal_pose.position.y = 0.25
-    goal_pose.position.z = 1.48
-    q = quaternion_from_euler(0,0,0)
-    goal_pose.orientation.x = q[0]
-    goal_pose.orientation.y = q[1]
-    goal_pose.orientation.z = q[2]
-    goal_pose.orientation.w = q[3]
-    print(goal_pose)
-    left_move_group.set_pose_target(goal_pose)
-    plan = left_move_group.plan()
-    if(plan):
-        rospy.loginfo("got plan")
-    else:
-        rospy.loginfo("failed to get plan")
-    left_move_group.execute(plan,wait=True)
-    left_move_group.stop()
+    rospy.init_node("arm_interface_action_server")
+    joint_action_server = JointGoalPlanner()
+    pose_action_server = PoseGoalPlanner()
+    rospy.loginfo("Arms Interface Action Server Initialized")
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("keyboard interupt detected, exiting...")
+        exit()
+    # moveit_commander.roscpp_initialize(sys.argv)
+    # rospy.init_node('raya_arms_interface', anonymous=True)
+    # robot = RobotCommander()
+    # scene = PlanningSceneInterface()
+    # left_move_group = MoveGroupCommander("left_arm")
+    # right_move_group = MoveGroupCommander("right_arm")
+    # goal_pose = Pose()
+    # goal_pose.position.x = 0.28
+    # goal_pose.position.y = 0.25
+    # goal_pose.position.z = 1.48
+    # q = quaternion_from_euler(0,0,0)
+    # goal_pose.orientation.x = q[0]
+    # goal_pose.orientation.y = q[1]
+    # goal_pose.orientation.z = q[2]
+    # goal_pose.orientation.w = q[3]
+    # print(goal_pose)
+    # left_move_group.set_pose_target(goal_pose)
+    # plan = left_move_group.plan()
+    # if(plan):
+    #     rospy.loginfo("got plan")
+    # else:
+    #     rospy.loginfo("failed to get plan")
+    # left_move_group.execute(plan,wait=True)
+    # left_move_group.stop()
